0a63f56097db05aca1a796101c37b8982ca625b6
Kshitij Goel <kshitijg9899@gmail.com>
2018-06-10 22:32:01 +0530

    Final Todo List

--- Todo.js
+++ Todo.js
@@ -1,33 +1,26 @@
-striked();
 dele();
 var inp = document.getElementById("input");
-inp.addEventListener("keypress", function(event) {
+inp.addEventListener("keydown", function(event) {
 	if(event.key == "Enter") {
 		var ul = document.getElementById("ul");
 		var li = document.createElement("li");
 		var i = document.createElement("i");
 		var span = document.createElement("span");
+		var checkbox = document.createElement("input");
+		checkbox.checked=false;
+		checkbox.setAttribute("type", "checkbox");
+		checkbox.onclick="strike();"
 		i.className="fa fa-trash";
 		span.appendChild(i);
+		li.appendChild(checkbox);
 		li.appendChild(span);
-		li.appendChild(document.createTextNode(inp.value));
+		li.appendChild(document.createTextNode(" " + inp.value));
 		li.className="add";
 		ul.appendChild(li);
 		inp.value="";
-		striked();
 		dele();
 	};
 });
-
-function striked() {
-	var strike = document.getElementsByTagName("li");
-	for(var i=0;i<strike.length;i++) {
-		strike[i].addEventListener("click", function() {
-			this.classList.remove("add");
-			this.classList.toggle("selected");
-		});
-	};
-};
 function dele() {
 	var close = document.getElementsByTagName("span");
 	for(var i=0;i<close.length;i++) {
@@ -36,3 +29,38 @@ function dele() {
 		});
 	};
 };
+// striked();
+// dele();
+// var inp = document.getElementById("input");
+// inp.addEventListener("keypress", function(event) {
+// 	if(event.key == "Enter") {
+// 		var ul = document.getElementById("ul");
+// 		var li = document.createElement("li");
+// 		var i = document.createElement("i");
+// 		var span = document.createElement("span");
+// 		i.className="fa fa-trash";
+// 		i.setAttribute("onclick","dele()")
+// 		span.appendChild(i);
+// 		li.appendChild(span);
+// 		li.appendChild(document.createTextNode(inp.value));
+// 		li.setAttribute("onclick","striked()");
+// 		ul.appendChild(li);
+// 		inp.value="";
+// 	};
+// });
+// function striked() {
+// 	var strike = document.getElementsByTagName("li");
+// 	for(var i=0;i<strike.length;i++) {
+// 		strike[i].addEventListener("click", function() {
+// 			this.classList.toggle("selected");
+// 		});
+// 	};
+// };
+// function dele() {
+// 	var close = document.getElementsByTagName("span");
+// 	for(var i=0;i<close.length;i++) {
+// 		close[i].addEventListener("click", function() {
+// 			this.parentNode.style.display="none";
+// 		});
+// 	};
+// };
