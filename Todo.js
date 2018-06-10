3517aa3a515e7f86945a7c0076ce3fee6771b02d
Kshitij Goel <kshitijg9899@gmail.com>
2018-06-10 18:51:58 +0530

    Final

--- Todo.js
+++ Todo.js
@@ -1,27 +1,38 @@
-var close = document.getElementsByTagName("span");
-for(var i=0;i<close.length;i++) {
-	close[i].addEventListener("click", function() {
-		this.parentNode.style.display="none";
-	});
-};
-var strike = document.getElementsByTagName("li");
-for(var i=0;i<strike.length;i++) {
-	strike[i].addEventListener("click", function() {
-		this.classList.toggle("selected");
-	});
-};
+striked();
+dele();
 var inp = document.getElementById("input");
 inp.addEventListener("keypress", function(event) {
 	if(event.key == "Enter") {
-		// var ul = document.getElementById("ul");
-		// var li = document.createElement("li");
-		// li.appendChild(document.createTextNode(inp.value));
-		// var span = document.createElement("span");
-		// span.className = "fa fa-times close";
-		// li.appendChild(span);
-		// ul.appendChild(li);
-		this.append("<li><span><i class='fa fa-times close'></i></span>" + inp.value + "</li>");
+		var ul = document.getElementById("ul");
+		var li = document.createElement("li");
+		var i = document.createElement("i");
+		var span = document.createElement("span");
+		i.className="fa fa-trash";
+		span.appendChild(i);
+		li.appendChild(span);
+		li.appendChild(document.createTextNode(inp.value));
+		li.className="add";
+		ul.appendChild(li);
 		inp.value="";
-
-	}
+		striked();
+		dele();
+	};
 });
+
+function striked() {
+	var strike = document.getElementsByTagName("li");
+	for(var i=0;i<strike.length;i++) {
+		strike[i].addEventListener("click", function() {
+			this.classList.remove("add");
+			this.classList.toggle("selected");
+		});
+	};
+};
+function dele() {
+	var close = document.getElementsByTagName("span");
+	for(var i=0;i<close.length;i++) {
+		close[i].addEventListener("click", function() {
+			this.parentNode.style.display="none";
+		});
+	};
+};
